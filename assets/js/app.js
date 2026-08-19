abcc42ca91c329e216637b5dd262c4f15a96ac9c
JiangCheng <jiangcheng_2004@163.com>
2026-08-19 15:36:48 +0800

    fix: 模块 import 加版本参数绕过 GitHub Pages 10 分钟缓存

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,8 +9,8 @@ import { passwordStrength } from './crypto.js';
 import { mountTechCanvas } from './bg-canvas.js';
 import * as gh from './github.js';
 import * as router from './router.js';
-import * as views from './views.js';
-import { renderAdmin } from './editor.js';
+import * as views from './views.js?v=20260819a';
+import { renderAdmin } from './editor.js?v=20260819a';
 
 const app = () => document.getElementById('app');
 
